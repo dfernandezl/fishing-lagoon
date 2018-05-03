848750e85319481de0c7c1f6ad47ee6e10fb8360
David Rodenas <david.rodenas@gmail.com>
2018-05-03 17:40:43 +0200

    create initial bot tokens if FISHING_LAGOON_GENERATE_* environment variables available

--- src/main/java/com/drpicox/fishingLagoon/MainServer.java
+++ src/main/java/com/drpicox/fishingLagoon/MainServer.java
@@ -1,16 +1,46 @@
 package com.drpicox.fishingLagoon;
 
 import com.drpicox.fishingLagoon.admin.AdminToken;
+import com.drpicox.fishingLagoon.bots.BotToken;
 
 import java.sql.SQLException;
 
 public class MainServer {
 
     public static void main(String[] args) throws SQLException {
-        var adminToken = System.getenv("FISHING_LAGOON_ADMIN_TOKEN");
+        var adminTokenString = System.getenv("FISHING_LAGOON_ADMIN_TOKEN");
+        var adminToken = new AdminToken(adminTokenString);
 
-        var bootstrap = new Bootstrap(new AdminToken(adminToken));
+        var bootstrap = new Bootstrap(adminToken);
         var restController = bootstrap.getRestController();
+
+        generateBotTokens(bootstrap.getGameController(), adminToken);
+
         restController.start();
+    }
+
+    private static void generateBotTokens(GameController gameController, AdminToken adminToken) {
+        var common = System.getenv("FISHING_LAGOON_GENERATE_COMMON");
+        var tokensString = System.getenv("FISHING_LAGOON_GENERATE_TOKENS");
+
+        if (common == null || tokensString == null) return;
+
+        var tokens = tokensString.split(",");
+        for (var token: tokens) {
+            generateBotToken(common, token + "-0", gameController, adminToken);
+            generateBotToken(common, token + "-1", gameController, adminToken);
+            generateBotToken(common, token + "-2", gameController, adminToken);
+        }
+    }
+
+    private static void generateBotToken(String common, String token, GameController gameController, AdminToken adminToken) {
+        try {
+            var botToken = new BotToken(common + "-" + token);
+            if (gameController.getBotByToken(botToken) == null) {
+                gameController.createBot(botToken, adminToken);
+            }
+        } catch (Throwable th) {
+            th.printStackTrace();
+        }
     }
 }
